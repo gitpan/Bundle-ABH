fe29c960a33e698ad98317d895aa9022789a7c77
Ask Bjørn Hansen <ask@perl.org>
1999-08-11 19:03:33 +0000

    Import of ABH/Bundle-ABH-0.01 from CPAN.
    gitpan-cpan-distribution: Bundle-ABH
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         ABH/Bundle-ABH-0.01.tar.gz
    gitpan-cpan-author:       ABH
    gitpan-cpan-maturity:     released

---
+++ ABH.pm
@@ -0,0 +1,84 @@
+package Bundle::ABH;
+
+$VERSION = "0.01";
+#$VERSION = sprintf("%d.%02d", q$Revision: 1.0 $ =~ /(\d+)\.(\d+)/);   
+
+1;
+
+__END__
+
+
+=head1 NAME
+
+Bundle::ABH - A bundle to install all Ask's favorite modules
+
+=head1 SYNOPSIS
+
+ perl -MCPAN -e 'install Bundle::ABH'
+
+=head1 CONTENTS
+
+Bundle::CPAN
+
+Bundle::libnet
+
+Bundle::LWP       
+
+CGI
+
+ExtUtils::Embed
+
+Mail::Send
+
+Getopt::Long
+
+DB_File
+
+Bundle::DBI
+
+Devel::Peek
+
+Tie::Handle
+
+Time::HiRes
+
+GD
+
+Storable
+
+HTML::Parser
+
+Data::ShowTable
+
+LWP::Parallel
+
+TimeDate
+
+Tie::IxHash
+
+Devel::Symdump
+
+Image::Size
+
+Convert::Translit
+
+Text::Soundex
+
+Pod::Parser
+
+HTML::TreeBuilder
+
+Net::DNS
+
+Text::Template
+
+=head1 DESCRIPTION
+
+This bundle defines most of the modules Ask Bjoern Hansen usually
+install/upgrade with CPAN.pm.
+
+=head1 AUTHOR
+
+Ask Bjoern Hansen E<lt>ask@netcetera.dk>
+
+=cut 
